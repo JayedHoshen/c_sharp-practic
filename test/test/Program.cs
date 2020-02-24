83d716040e11b5fd97111878256446fccb43aff1
MJ <jayedhoshen>
2020-02-24 08:28:58 +0600

    Added all file first time

---
+++ test/test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test
+{
+    class test_one {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+
+}
