c5483200a692af459e292e1ef066424e1498c803
MJ <jayedhoshen>
2020-02-24 12:05:30 +0600

    add cSharp typeConversion file

---
+++ typeConvertion/typeConvertion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace typeConversion {
+    class program {
+        static void Main(string[] args) {
+            int num1 = 20000;
+            int num2 = 50000;
+            int num3 = 65;
+            long total;
+            char alpha;
+            alpha = (char)num3;
+            total = num1 + num2;
+            Console.WriteLine($"Total is: {total}");
+        }
+    }
+}
